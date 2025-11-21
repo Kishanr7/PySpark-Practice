3fb6241c1d97f144700ce11d4bd5bbca79a9b480
Kishanr7 <kishanrekhadia79@gmail.com>
2025-11-21 18:03:52 +0530

    top 3 emp per dept

---
+++ 21-11-2025/top_3_employees_per_department.py
@@ -0,0 +1,33 @@
+# Second Approach
+from pyspark.sql import SparkSession
+from pyspark.sql.types import StructType, StructField, IntegerType, StringType, DoubleType
+from pyspark.sql import functions as F
+from pyspark.sql.window import Window
+from pyspark.sql.functions import datediff, col, lag, row_number, rank, dense_rank, sum, upper, round as rnd
+
+spark = SparkSession.builder.getOrCreate()
+
+data = [
+    ("Alice", "HR", 50000),
+    ("Bob", "HR", 60000),
+    ("Charlie", "HR", 55000),
+    ("David", "IT", 70000),
+    ("Eve", "IT", 72000),
+    ("Frank", "IT", 71000),
+    ("Grace", "Finance", 65000),
+    ("Heidi", "Finance", 64000),
+]
+
+columns = ["employee_name", "department", "salary"]
+
+df = spark.createDataFrame(data, columns)
+
+w = (Window
+     .partitionBy("department")
+     .orderBy(F.col("salary").desc())
+)
+final = (df
+         .withColumn("rnk", F.row_number().over(w))
+         .filter(F.col("rnk") <= 3)
+)
+final.show(truncate=False)
